f7e5fcdd1eeda5ab8029e9ffbd559610eca1ffc1
Mohammad Sahihi <msahihi1@gwdg.de>
2017-12-11 12:59:29 +0100

    added column_list to read_data_frame func

--- dataframe.py
+++ dataframe.py
@@ -23,29 +23,28 @@ class DataFrame(object):
                 filtered_list[file_name] = file_path
         return filtered_list
 
-    def read_data_frame(self, file_name):
+    def read_data_frame(self, file_name, columns_list):
         data_frame = pd.read_csv(file_name, index_col=0, parse_dates=True)
-        column_name_with_postfix = file_name.split('/')[-1]
-        column_name = column_name_with_postfix.split('.')[0]
-        column_value = {'value': column_name}
-        data_frame.rename(columns=column_value, inplace=True)
+        file_name_with_postfix = file_name.split('/')[-1]
+        file_name_as_prefix = file_name_with_postfix.split('.')[0]
+        data_frame = data_frame[columns_list]
+        for column in columns_list:
+            column_value = {column: file_name_as_prefix + '_' + column}
+            data_frame.rename(columns=column_value, inplace=True)
         return data_frame
 
     def merge_data_frames(self, data_from_list):
         all_series = []
-        item = 0
         for data_frame in data_from_list:
-            item += 1
-            data_frame.drop(data_frame.columns[0:1], axis=1, inplace=True)
             all_series.append(data_frame)
         merged_dataframe = pd.concat(all_series, axis=1)
         merged_dataframe.dropna(inplace=True)
         return merged_dataframe
 
-    def merge_data_into_timeseries(self):
+    def merge_data_into_timeseries(self, columns_list):
         files = self.list_files(self.path, features=self.features)
         data_frame_list = []
         for name, path in files.items():
-            raw_data = self.read_data_frame(path)
+            raw_data = self.read_data_frame(path, columns_list)
             data_frame_list.append(raw_data)
         self.data = self.merge_data_frames(data_frame_list)
